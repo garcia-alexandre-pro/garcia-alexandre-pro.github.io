27fa074a476fabf85a5d0beba4a18daf189ae88a
garcia-alexandre-pro <67734538+garcia-alexandre-pro@users.noreply.github.com>
2025-05-05 16:58:05 +0200

    Content

--- js/ripple.js
+++ js/ripple.js
@@ -8,15 +8,16 @@ $(document).ready(function (e) {
     });
 
     new MutationObserver(function (mutations) {
-        var selector = ".btn";
+        const selector = ".btn";
+
         // look through all mutations that just occured
-        for (var i = 0; i < mutations.length; ++i) {
+        for (let i = 0; i < mutations.length; ++i) {
             // look through all added nodes of this mutation
-            for (var j = 0; j < mutations[i].addedNodes.length; ++j) {
-                var target = mutations[i].addedNodes[j];
+            for (let j = 0; j < mutations[i].addedNodes.length; ++j) {
+                let target = mutations[i].addedNodes[j];
 
                 if (target && target.nodeType === Node.ELEMENT_NODE) {
-                    var children = $(target).find(selector);
+                    let children = $(target).find(selector);
 
                     if (target.matches(selector)) {
                         if ($(target).children(".ripple").length === 0) {
@@ -41,7 +42,7 @@ $(document).ready(function (e) {
     new MutationObserver(function (mutations) {
         mutations.forEach(function (mutation) {
             if (mutation.attributeName === "class") {
-                //var attributeValue = $(mutation.target).prop(mutation.attributeName);
+                //let attributeValue = $(mutation.target).prop(mutation.attributeName);
                 if ($(mutation.target).hasClass("btn") && $(mutation.target).children(".ripple").length === 0) {
                     $(mutation.target).append(rippleHtml);
                 }
@@ -55,13 +56,13 @@ $(document).ready(function (e) {
 
 (function ($, window, document, undefined) {
     $(document).on('click', '.btn', function (e) {
-        var $ripple = $(this).find(".ripple");
-        var $offset = $ripple.parent().offset();
-        var $circle = $ripple.find('.ripple-circle');
+        let $ripple = $(this).find(".ripple");
+        let $offset = $ripple.parent().offset();
+        let $circle = $ripple.find('.ripple-circle');
 
         if ($ripple && $offset && $circle) {
-            var x = e.pageX - $offset.left;
-            var y = e.pageY - $offset.top;
+            let x = e.pageX - $offset.left;
+            let y = e.pageY - $offset.top;
 
             $circle.css({
                 top: y + 'px',
